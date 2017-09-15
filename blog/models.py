3f0840a52e58af3dbc55f125b2b215f35609fa4e
Chang Minwook <fromme0528@gmail.com>
2017-09-15 10:53:11 +0900

    Changs Database

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	create_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True,null=True)
 
 	def publish(self):
@@ -14,5 +14,3 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
-
-
