238d0a63bb74a7893f4dde1c95436c60afb45e0f
Chang Minwook <fromme0528@gmail.com>
2017-09-21 20:45:19 +0900

    fixed

--- blog/models.py
+++ blog/models.py
@@ -27,9 +27,3 @@ class Video(models.Model):
 	def __str__(self):
 		return self.title
 
-class Test(models.Model):
-	title = models.CharField(max_length=200)
-	text = models.TextField()
-
-	def __str__(self):
-		return self.title
